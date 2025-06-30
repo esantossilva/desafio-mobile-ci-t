df3bf6257fe282f37168105367fba47370e1dcae
DESKTOP-TDSET4L\eduar <eduardo@daccord.com.br>
2025-06-30 17:15:55 -0300

    feat: Adds movie credits to viewmodel.

--- MoviesDBChallenge/app/src/main/java/com/example/moviesdbchallenge/viewmodels/MovieDetailsViewModel.kt
+++ MoviesDBChallenge/app/src/main/java/com/example/moviesdbchallenge/viewmodels/MovieDetailsViewModel.kt
@@ -3,8 +3,10 @@ package com.example.moviesdbchallenge.viewmodels
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.models.CastModel
 import com.example.domain.models.DetailedMovieModel
 import com.example.domain.models.MovieRecommendationsModel
+import com.example.usecases.GetMovieCreditsUseCase
 import com.example.usecases.GetMovieDetailsUseCase
 import com.example.usecases.GetMovieRecommendationsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,6 +19,7 @@ import javax.inject.Inject
 class MovieDetailsViewModel @Inject constructor(
     private val getMovieDetailsUseCase: GetMovieDetailsUseCase,
     private val getMovieRecommendationsUseCase: GetMovieRecommendationsUseCase,
+    private val getMovieCreditsUseCase: GetMovieCreditsUseCase,
     savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
 
@@ -31,9 +34,13 @@ class MovieDetailsViewModel @Inject constructor(
     private val _recommendations = MutableStateFlow(MovieRecommendationsModel())
     val recommendations: StateFlow<MovieRecommendationsModel> = _recommendations
 
+    private val _cast = MutableStateFlow(emptyList<CastModel>())
+    val cast: StateFlow<List<CastModel>> = _cast
+
     init {
         getMovieDetails()
         getMovieRecommendations()
+        getMovieCredits()
     }
 
     private fun getMovieDetails() {
@@ -48,8 +55,16 @@ class MovieDetailsViewModel @Inject constructor(
         viewModelScope.launch {
             getMovieRecommendationsUseCase(movieId).collect { recommendations ->
                 _recommendations.value = recommendations
+            }
+        }
+    }
 
-                if (recommendations.results.isNotEmpty()) _loadingContent.value = false
+    private fun getMovieCredits() {
+        viewModelScope.launch {
+            getMovieCreditsUseCase(movieId).collect { credits ->
+                _cast.value = credits.cast
+
+                if (credits.cast.isNotEmpty()) _loadingContent.value = false
             }
         }
     }
